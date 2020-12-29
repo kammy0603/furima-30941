289e5ccbf69138e111f1c0c926ad852e9ec4192c
kammy0603 <wingzero0127@gmail.com>
2020-12-29 15:15:16 +0900

    ユーザー管理機能の実装_テストコード実施_3

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
   validates :nickname, presence: true
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
-    validates :family_name_kanji, presence: true
-    validates :first_name_kanji, presence: true
+    validates :family_name_kanji,
+    validates :first_name_kanji,
   end
   with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カナ文字を使用してください' } do
-    validates :family_name_kana, presence: true
-    validates :first_name_kana, presence: true
+    validates :family_name_kana,
+    validates :first_name_kana,
   end
   validates :birthday, presence: true
 end
