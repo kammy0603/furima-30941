7c1de355f1827259d5b42fa7699af392f9e49246
kammy0603 <wingzero0127@gmail.com>
2020-12-29 15:02:42 +0900

    ユーザー管理機能の実装_テストコード実施_2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,14 +2,14 @@ class ItemsController < ApplicationController
   def index
   end
 
-  def new
-    @user = User.new
-  end
+  #def new
+    #@user = User.new
+  #end
 
-  def create
-    @user = User.create(user_params)
-  end
+  #def create
+    #@user = User.create(user_params)
+  #end
 
-  def destroy
-  end
+  #def destroy
+  #end
 end
