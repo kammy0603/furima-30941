18a1e45c8d258ba9be46498503cfcbd7d1b246de
kammy0603 <wingzero0127@gmail.com>
2021-01-01 22:22:58 +0900

    商品出品機能を実装_2

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,17 +2,12 @@ FactoryBot.define do
   factory :item do
     name                 { Faker::Name.last_name }
     introduction         { Faker::Lorem.sentence }
-    #category_id          { '2' }
-    #item_status_id       { '3' }
-    #distribution_fee_id  { '4' }
     region_id            { '5' }
-    #distribution_term_id { '6' }
     price                { '1000' }
     user_id              { '1' }
     genre_id             { '2' }
     status_id            { '2' }
     distribution_id      { '2' }
-    #region_genre_id      { '2' }
     term_id              { '2' }
     association :user
 
