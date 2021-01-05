45162dab09d2f71a19f0129ff4bcfc0b01325ef3
kammy0603 <wingzero0127@gmail.com>
2021-01-05 22:20:30 +0900

    商品一覧表示機能の実装_3

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :genre
   belongs_to_active_hash :status
-  belongs_to_active_hash :distribution_fee
+  belongs_to_active_hash :distribution
   belongs_to_active_hash :region
   belongs_to_active_hash :term
 
